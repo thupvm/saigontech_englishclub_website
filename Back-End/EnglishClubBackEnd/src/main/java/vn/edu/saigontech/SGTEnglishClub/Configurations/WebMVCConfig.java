35abcf7996425e59a5ec3dea1d54ef8b7c581c90
Minh Quan <nguyenquan263@gmail.com>
2018-11-04 17:26:48 +0700

    Edit static resources

--- Back-End/EnglishClubBackEnd/src/main/java/vn/edu/saigontech/SGTEnglishClub/Configurations/WebMVCConfig.java
+++ Back-End/EnglishClubBackEnd/src/main/java/vn/edu/saigontech/SGTEnglishClub/Configurations/WebMVCConfig.java
@@ -16,9 +16,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMVCConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
-		registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
-		registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+		registry.addResourceHandler("/assets/**").addResourceLocations("/assets/").setCachePeriod(31556926);
+
 	}
 
 	@Override
